a045f7bca5c62bd49547d0192b91c1f90d09df51
Liliana Martins <lilianasrmartins@gmail.com>
2020-01-25 15:56:22 +0000

    added seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_list = open(url).read
 ingredients_array = JSON.parse(ingredients_list)
 
-ingredient_array['drinks'].each do |ingredient|
+ingredients_array['drinks'].each do |ingredient|
+  # puts ingredient['strIngredient1']
   Ingredient.create(name: ingredient['strIngredient1'])
 end
