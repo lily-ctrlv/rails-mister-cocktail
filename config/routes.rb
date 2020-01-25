e8f960c7b8cd59470694d2c510ee5b16a0c6851f
Liliana Martins <lilianasrmartins@gmail.com>
2020-01-25 17:15:36 +0000

    almost there with routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # read all
+  get "cocktails", to: "cocktails#index"
+
+  # read show
+
+  get "cocktails/:id", to: "cocktails#show"
+
+  # create one
+
+  get "cocktails/new", to: "cocktails#new"
+  post "cocktails", to: "cocktails#create"
+
 end
