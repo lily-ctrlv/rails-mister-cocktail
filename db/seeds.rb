288d9b781b1aa8688ecb58b5d0fb931ff3890bc7
Liliana Martins <lilianasrmartins@gmail.com>
2020-01-25 15:51:39 +0000

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_list = open(url).read
+ingredients_array = JSON.parse(ingredients_list)
+
+ingredient_array['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
